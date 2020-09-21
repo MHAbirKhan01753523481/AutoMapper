8d961d27eb63223058a5dc88af16c19969691df8
MHAbirKhan01753523481 <71643559+MHAbirKhan01753523481@users.noreply.github.com>
2020-09-22 01:28:14 +0600

    Add files via upload

---
+++ AutoMapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using AutoMapper2._1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoMapper2._1.AutoMapper
+{
+    public class AutoMapper:Profile
+    {
+        public AutoMapper()
+        {
+            CreateMap<vmCreate, Student>();
+            CreateMap<Student, vmList>();
+
+            CreateMap<vmEdit, Student>();
+            CreateMap<Student, vmDetails>();
+        }
+    }
+}
