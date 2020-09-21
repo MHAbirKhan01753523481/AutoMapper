9977ba3ab29639c54dc5f3a0e4bed20a8f736962
MHAbirKhan01753523481 <71643559+MHAbirKhan01753523481@users.noreply.github.com>
2020-09-22 01:27:33 +0600

    Add files via upload

---
+++ StudentsController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using AutoMapper2._1.Data;
+using AutoMapper2._1.Models;
+using AutoMapper;
+
+namespace AutoMapper2._1.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly studentDbContext _context;
+        private readonly IMapper _iMapper;
+
+        public StudentsController(studentDbContext context,IMapper mapper)
+        {
+            _context = context;
+            _iMapper = mapper;
+        }
+
+        // GET: Students
+        public IActionResult Index()
+        {
+            var student=_context.Student.ToList();
+
+            var stud = _iMapper.Map<IEnumerable<vmList>>(student);
+
+            return View(stud);
+        }
+
+        // GET: Students/Details/5
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = _context.Student
+                .FirstOrDefault(m => m.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // GET: Students/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(vmCreate vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var student = _iMapper.Map<Student>(vm);
+                _context.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vm);
+        }
+
+        // GET: Students/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student =_context.Student.Find(id);
+            var studentobj = _iMapper.Map<Student>(student);
+         
+            if (studentobj == null)
+            {
+                return NotFound();
+            }
+            return View(studentobj);
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, vmEdit student)
+        {
+            if (id != student.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var studentEdit = _iMapper.Map<Student>(student);
+                try
+                {
+                    _context.Update(studentEdit);
+                    _context.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentExists(studentEdit.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(student);
+        }
+
+        // GET: Students/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Student
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        // POST: Students/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var student = await _context.Student.FindAsync(id);
+            _context.Student.Remove(student);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StudentExists(int id)
+        {
+            return _context.Student.Any(e => e.Id == id);
+        }
+    }
+}
